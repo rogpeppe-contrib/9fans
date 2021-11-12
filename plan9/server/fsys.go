df9730ae5dcdf13f04e7a3864f5194bdea52c8b6
Roger Peppe <rogpeppe@gmail.com>
2021-11-12 08:27:54 +0000

    plan9/server: clarify concurrency guarantees of Fsys type

--- plan9/server/fsys.go
+++ plan9/server/fsys.go
@@ -32,26 +32,47 @@ type Fsys[F Fid] interface {
 	// in use, it will be discarded by calling Clunk.
 	//
 	// A fid that's been opened will never be cloned.
+	//
+	// This method must be safe to call concurrently.
 	Clone(f F) F
 
 	// Clunk discards an instance of F. Clunk will never be called while there are any running
 	// I/O methods on f.
+	//
+	// This method will never be called concurrently on the same instance of f.
 	Clunk(f F)
 
-	// Auth returns a new auth fid associated with the given user and attach name.
-	// TODO should the returned fid be considered open?
+	// Auth returns a new open auth fid associated with the given user and attach name.
+	// It must represent a file with the QTAUTH bit set.
+	//
+	// This method must be safe to call concurrently.
 	Auth(ctx context.Context, uname, aname string) (F, error)
 
 	// Attach attaches to a new tree, returning a new
 	// instance of F representing the root.
 	// If auth is non-nil, it holds the auth fid.
+	//
+	// This method must be safe to call concurrently.
 	Attach(ctx context.Context, auth *F, uname, aname string) (F, error)
 
+	// Stat returns information about the file, which may be open or not.
+	//
+	// This method must be safe to call concurrently.
 	Stat(ctx context.Context, f F) (plan9.Dir, error)
+
+	// Wstat writes metadata about the file.
+	//
+	// This method must be safe to call concurrently.
+	// TODO should we make this exclusive?
 	Wstat(ctx context.Context, f F, dir plan9.Dir) error
 
 	// Walk walks to the named element within the directory
 	// represented by f and returns a handle to that element.
+	// It's OK for Walk to return the same f (i.e. just update
+	// the file represented by f in place). If it returns a different
+	// value, the original value will be clunked immediately.
+	//
+	// This method will never be called concurrently on the same f.
 	Walk(ctx context.Context, f F, name string) (F, error)
 
 	// Open prepares a fid for I/O.
@@ -59,19 +80,36 @@ type Fsys[F Fid] interface {
 	// than Readdir (if it's a directory), ReadAt or WriteAt (if it's a file)
 	// or Clunk.
 	// Open returns the opened file and the its associated iounit.
+	//
+	// It's OK for Open to return the same f (i.e. just update
+	// the file represented by f in place). If it returns a different
+	// value, the original value will be clunked immediately.
+	//
+	// This method will never be called concurrently on the same f.
 	Open(ctx context.Context, f F, mode uint8) (F, uint32, error)
 
 	// Readdir reads directory entries from an open directory into
 	// dir, starting at the number of entries into the directory.
 	// It returns the number of entries read.
+	//
+	// This method will never be called concurrently on the same f.
 	Readdir(ctx context.Context, f F, dir []plan9.Dir, entryIndex int) (int, error)
 
+	// ReadAt reads data from f into buf at the file offset off.
+	//
+	// This method must be safe to call concurrently.
 	ReadAt(ctx context.Context, f F, buf []byte, off int64) (int, error)
+
+	// WriteAt writes data from buf into f at the file offset off.
+	//
+	// This method must be safe to call concurrently.
 	WriteAt(ctx context.Context, f F, buf []byte, off int64) (int, error)
 
 	// Remove removes the file represented by f. Unlike 9p remove,
 	// this does not imply a clunk - the Clunk method will be explicitly
 	// called immediately after Remove.
+	//
+	// This method will never be called concurrently on the same f.
 	Remove(ctx context.Context, f F) error
 
 	// Close is called when the entire server instance is being torn down.
